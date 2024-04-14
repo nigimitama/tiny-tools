ab36f92229fd92b9f9633f5a79889b732a258dad
nigimitama <nigimitama16@gmail.com>
2024-04-14 20:53:52 +0900

    refactor: remove space

--- src/routes/space-remover.tsx
+++ src/routes/space-remover.tsx
@@ -4,7 +4,7 @@ import { Field, Textarea } from "@fluentui/react-components";
 
 
 const removeSpaces = (text: string) => {
-  const space = /[ ​\u00a0\u1680​\u180e\u2000​\u200a​\u2028\u2029\u202f\u205f​\u3000]/g;
+  const space = /[ \u00a0\u1680\u180e\u2000\u200a\u2028\u2029\u202f\u205f\u3000]/g;
   return text.replace(space, '');
 }
 
